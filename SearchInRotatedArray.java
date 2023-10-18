ecedbb778a47a0903d2d76687775e1ade3c75357
karl doherty <karldoherty15@icloud.com>
2023-10-18 22:11:19 +0100

    Todays submissions

--- SearchInRotatedArray.java
+++ SearchInRotatedArray.java
@@ -4,48 +4,49 @@
 
 public class SearchInRotatedArray {
     public static void main(String[] args) {
-        int[] nums = { 5,1,3 };
-        int target = 0;
+        int[] nums = { 6, 7, 1, 2, 3, 4, 5 };
+        int target = 6;
         System.out.println(search(nums, target));
     }
 
     public static int search(int[] nums, int target) {
-        if(nums.length == 1) {
-            if(target == nums[0]) {
-                return 0;
-            } else {
-                return -1;
-            }
-        }
         int peak = findPeak(nums);
-        System.out.println("Peak ->" + peak);
-        int ascending = binarySearch(nums, target, 0, peak);
-        if (ascending == -1) {
-            return binarySearch(nums, target, peak + 1, nums.length - 1);
+        // if you did not find a pivot, the array is not rotated so just do a normal
+        // binary search
+        if (peak == -1) {
+            return binarySearch(nums, target, 0, nums.length - 1);
+        } else {
+            if (binarySearch(nums, target, 0, peak) == -1) {
+                return binarySearch(nums, target, peak + 1, nums.length - 1);
+            }
+            return binarySearch(nums, target, 0, peak);
         }
-        return ascending;
     }
 
     public static int findPeak(int[] nums) {
         int start = 0;
         int end = nums.length - 1;
 
-        while (start < end) {
+        while (start <= end) {
             int mid = start + (end - start) / 2;
-            if (nums[mid] > nums[mid + 1]) {
+            if (mid < end && nums[mid] > nums[mid + 1]) {
                 // if the middle element is larger than the element after it, it is the pivot
                 // point
                 return mid;
-            } else if (nums[start] > nums[mid]) {
-                // if the start element is larger than the middle element, the ascending part of
-                // the array comes before the middle element so put end at mid - 1
+            } else if (mid > start && nums[mid] < nums[mid - 1]) {
+                // if the middle element is smaller than the element before it, the one before
+                // it is the pivot element
+                return mid - 1;
+            } else if (nums[mid] < nums[start]) {
+                // if the middle element is less than start, move end back to mid - 1 as the
+                // ascending part is before middle
                 end = mid - 1;
             } else {
-                // otherwise, move start to middle + 1
                 start = mid + 1;
             }
         }
-        return start;
+        // return -1 if there is no pivot element, meaning the array is not rotated
+        return -1;
     }
 
     public static int binarySearch(int[] arr, int target, int start, int end) {
